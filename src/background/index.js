368a25e52d61bda255a10cef8debc60d888bdf14
Kuzirashi <kuzirashi@interia.pl>
2019-03-25 14:09:29 +0100

    Use page action instead of browser action.

--- src/background/index.js
+++ src/background/index.js
@@ -15,6 +15,19 @@ chrome.runtime.onInstalled.addListener(function() {
       }
     });
   });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'twitter.com' }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
 
 // Standard Google Universal Analytics code
